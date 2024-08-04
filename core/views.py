c14e7f9d390d2725ce1e8615360e16d258cdb562
Linlin Yan <linlin.yan@bioinfo.app>
2024-08-04 18:31:25 +0800

    Implement query function.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from core.models import Paper
 
 def get_paginated_reviews(reviews, page_number):
@@ -22,7 +23,29 @@ def get_paginated_reviews(reviews, page_number):
     return reviews, zip(items, indices)
 
 def home(request):
-    papers = Paper.objects.all().order_by('-created')
+    papers = Paper.objects.all()
+
+    query = request.GET.get('q') or ''
+    if query:
+        papers = Paper.objects.filter(
+            Q(title__icontains=query) |
+            Q(journal__icontains=query) |
+            Q(doi = query) |
+            Q(pmid = query) |
+            Q(article_type__icontains=query) |
+            Q(description__icontains=query) |
+            Q(novelty__icontains=query) |
+            Q(limitation__icontains=query) |
+            Q(research_goal__icontains=query) |
+            Q(research_objects__icontains=query) |
+            Q(field_category__icontains=query) |
+            Q(disease_category__icontains=query) |
+            Q(technique__icontains=query) |
+            Q(model_type__icontains=query) |
+            Q(data_type__icontains=query) |
+            Q(sample_size__icontains=query))
+
+    papers = papers.order_by('-created')
 
     page_number = request.GET.get('page')
     papers, items = get_paginated_reviews(papers, page_number)
@@ -30,6 +53,7 @@ def home(request):
     for index, paper in enumerate(papers):
         paper.index = index + 1
     return render(request, 'core/home.html', {
+        'query': query,
         'papers': papers,
         'items': items,
     })
