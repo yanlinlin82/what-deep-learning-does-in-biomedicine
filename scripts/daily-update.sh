ba182a2a17b09ac05b553b545f9f4bb41d4b0768
Linlin Yan <linlin.yan@bioinfo.app>
2024-11-26 15:40:50 +0800

    Add script for daily update.

---
+++ scripts/daily-update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d pubmed/updatefiles/ ]; then
+    echo "ERROR: Directory 'pubmed/updatefiles/' does not exist!"
+    exit 1
+fi
+
+BAK_FILE=db.sqlite3.bak-$(date +%Y%m%d)
+if [ -e "${BAK_FILE}" ]; then
+	echo "It has been updated already today"
+	exit 0
+fi
+cp -av db.sqlite3 ${BAK_FILE}
+
+PUBMED_XML_GZ=$(find pubmed/updatefiles/ -type f -name '*.xml.gz' | sort | tail -n1)
+if [ -z "${PUBMED_XML_GZ}" ]; then
+    echo "ERROR: No PubMed XML file found in 'pubmed/updatefiles/'!"
+    exit 1
+fi
+echo "Latest PubMed XML file: $PUBMED_XML_GZ"
+
+SOURCE=$(basename $PUBMED_XML_GZ .xml.gz)
+SOURCE=${SOURCE#pubmed24n}
+echo "Got source: $SOURCE"
+
+mkdir -pv log/pubmed
+.venv/bin/python scripts/scan-pubmed.py $PUBMED_XML_GZ >>log/pubmed/${SOURCE}.log 2>&1
