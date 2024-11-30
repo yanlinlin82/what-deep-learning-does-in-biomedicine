97e6c95dff214369a4181ef2457a0409f39c5840
Linlin Yan <linlin.yan@bioinfo.app>
2024-11-30 09:47:58 +0800

    Add messages in daily update script.

--- scripts/daily-update.sh
+++ scripts/daily-update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+APP_NAME=$(basename $(pwd))
+echo "========== $(date) =========="
+echo ">>> Updating for $APP_NAME"
+
 if [ ! -d pubmed/updatefiles/ ]; then
     echo "ERROR: Directory 'pubmed/updatefiles/' does not exist!"
     exit 1
@@ -7,7 +11,7 @@ fi
 
 BAK_FILE=db.sqlite3.bak-$(date +%Y%m%d)
 if [ -e "${BAK_FILE}" ]; then
-	echo "It has been updated already today"
+	echo ">>> It has been updated already today"
 	exit 0
 fi
 cp -av db.sqlite3 ${BAK_FILE}
@@ -17,11 +21,13 @@ if [ -z "${PUBMED_XML_GZ}" ]; then
     echo "ERROR: No PubMed XML file found in 'pubmed/updatefiles/'!"
     exit 1
 fi
-echo "Latest PubMed XML file: $PUBMED_XML_GZ"
+echo ">>> Latest PubMed XML file: $PUBMED_XML_GZ"
 
 SOURCE=$(basename $PUBMED_XML_GZ .xml.gz)
 SOURCE=${SOURCE#pubmed24n}
-echo "Got source: $SOURCE"
+echo ">>> Got source: $SOURCE"
 
 mkdir -pv log/pubmed
 .venv/bin/python scripts/scan-pubmed.py $PUBMED_XML_GZ >>log/pubmed/${SOURCE}.log 2>&1
+
+echo ">>> Updating for $APP_NAME done"
